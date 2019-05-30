fd16c0c107124d57d8de5fad2b49488ab2f9617e
Tomoyuki Sugita <tomotomo@users.noreply.github.com>
2019-05-30 19:36:53 +0900

    Update jtalk.py

--- chapter3/jtalk.py
+++ chapter3/jtalk.py
@@ -1,8 +1,13 @@
 import argparse
 import subprocess
 
-
+'''
+発話させたい内容を受け取って音声ファイルを作成する関数
+- text : 喋らせたいテキスト
+- path : 音声ファイルの作成先
+'''
 def create_wave(text, path):
+    # テキストをUTF-8に変換
     if isinstance(text, bytes):
         text = text.decode('utf-8')
 
@@ -11,12 +16,14 @@ def create_wave(text, path):
     text = text.replace('\n', ' ')
     text = text.encode('utf-8')
 
+    # コマンドを作成する部分。必要なコマンドとその引数やオプションを繋げているだけです。
     open_jtalk = ['open_jtalk']
     mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
     htsvoice = ['-m', '/usr/share/hts-voice/mei/mei_normal.htsvoice']
     outwav = ['-ow', path]
     cmd = open_jtalk + mech + htsvoice + outwav
 
+    # 作成したコマンドを子プロセスとして実行
     c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
     c.stdin.write(text)
     c.stdin.close()
@@ -25,6 +32,7 @@ def create_wave(text, path):
     return path
 
 def play(path):
+    # aplayコマンドを実行する
     cmd = ['aplay', path]
     subprocess.call(cmd)
 
@@ -34,6 +42,7 @@ def jtalk(text, path='/tmp/jtalk.wav'):
 
 
 if __name__ == '__main__':
+    # コマンドの引数を受け取る
     ap = argparse.ArgumentParser()
     ap.add_argument('-t', '--text', required=True, help='発話させるテキスト')
     ap.add_argument('-p', '--path', default='/tmp/jtalk.wav',
